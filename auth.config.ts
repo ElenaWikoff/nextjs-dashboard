98ea6baf2f8b7a9d9054b98905c16e86528070a5
ElenaWikoff <elenawikoff@gmail.com>
2025-05-13 15:30:49 -0500

    Finished tutorial

---
+++ auth.config.ts
@@ -0,0 +1,22 @@
+import type { NextAuthConfig } from 'next-auth';
+
+export const authConfig = {
+    pages: {
+        signIn: '/login',
+    },
+    callbacks: {
+        authorized({ auth, request: { nextUrl } }) {
+            const isLoggedIn = !!auth?.user;
+            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+            if (isOnDashboard) {
+                if (isLoggedIn) return true;
+                return false; // Redirect unauthenticated users to login page
+            } else if (isLoggedIn) {
+                return Response.redirect(new URL('/dashboard', nextUrl));
+            }
+            return true;
+        },
+    },
+    providers: [], // Add providers with an empty array for now
+    secret: process.env.SECRET || '7355ff119604167b440b562fb7b2f0a5',
+} satisfies NextAuthConfig;
